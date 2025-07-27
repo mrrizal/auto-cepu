6f053b307116d35df2b0bcd07825ce8b76e6ab3e
muhrzl <muhammad.rizal@julofinance.com>
2025-07-27 20:31:56 +0700

    handle failed normalize code

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ from ingestion.normalizer import normalize_code
 
 # Configure the root logger
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
 )
 
@@ -57,43 +57,40 @@ async def review_code(
     if not code.strip():
         raise HTTPException(status_code=400, detail="Code snippet cannot be empty.")
 
-    code = normalize_code(code)
-    similar_code = indexing_service.query_similar_code(code)
-    from pprint import pprint
-    pprint(similar_code)
+    try:
+        code = normalize_code(code)
+        similar_code = indexing_service.query_similar_code(code)
+    except SyntaxError:
+        similar_code = []
+
+    logger.info("Generating review prompt for the provided code snippet.")
+    review_prompt = prompt_generator.generate_coding_style_prompt(code)
+
+    if not similar_code:
+        logger.info("No similar code snippets found.")
+        style_result = await code_reviewer.review(review_prompt)
+        duplication_result = None
+    else:
+        logger.debug(f"Found {len(similar_code)} similar code snippets.")
+        duplicate_code_check_prompt = prompt_generator.\
+            generate_code_duplication_check_prompt(code, similar_code)
+
+        duplication_result, style_result = await asyncio.gather(
+            code_reviewer.review(duplicate_code_check_prompt),
+            code_reviewer.review(review_prompt)
+        )
+
+    if not duplication_result:
+        summary = style_result
+    else:
+        summary = await code_reviewer.review(
+            prompt_generator.generate_summary_prompt(
+                style_result['response'], duplication_result['response']
+            )
+        )
+
     return CodeReviewResponse(
-        duplication_review="",
-        style_review="",
-        summary=""
+        duplication_review=duplication_result['response'] if duplication_result else "No duplication found.",
+        style_review=style_result['response'] if style_result else "No style issues found.",
+        summary=summary['response'] if summary else "No summary available."
     )
-    # logger.info("Generating review prompt for the provided code snippet.")
-    # review_prompt = prompt_generator.generate_coding_style_prompt(code)
-
-    # if not similar_code:
-    #     logger.info("No similar code snippets found.")
-    #     style_result = await code_reviewer.review(review_prompt)
-    #     duplication_result = None
-    # else:
-    #     logger.debug(f"Found {len(similar_code)} similar code snippets.")
-    #     duplicate_code_check_prompt = prompt_generator.\
-    #         generate_code_duplication_check_prompt(code, similar_code)
-
-    #     duplication_result, style_result = await asyncio.gather(
-    #         code_reviewer.review(duplicate_code_check_prompt),
-    #         code_reviewer.review(review_prompt)
-    #     )
-
-    # if not duplication_result:
-    #     summary = style_result['response']
-    # else:
-    #     summary = await code_reviewer.review(
-    #         prompt_generator.generate_summary_prompt(
-    #             style_result['response'], duplication_result['response']
-    #         )
-    #     )
-
-    # return CodeReviewResponse(
-    #     duplication_review=duplication_result['response'] if duplication_result else "No duplication found.",
-    #     style_review=style_result['response'] if style_result else "No style issues found.",
-    #     summary=summary['response'] if summary else "No summary available."
-    # )
